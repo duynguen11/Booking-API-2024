72cbec553f201f63047530530f9584d1df73b6ef
duynguen11 <duynguen11@gmail.com>
2024-04-13 22:47:17 +0700

    fix urlImage 10:06pm duynguen11

--- next.config.js
+++ next.config.js
@@ -2,8 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["*"], // Thêm các mẫu URL từ xa mà bạn muốn chấp nhận cho hình ảnh
+    domains: ["localhost"], // Thêm các mẫu URL từ xa mà bạn muốn chấp nhận cho hình ảnh
   },
+
+  remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "localhost",
+      port: "2024",
+      pathname: "/uploads/**",
+    },
+  ],
 };
 
 module.exports = nextConfig;
